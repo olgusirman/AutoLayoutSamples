0cf443976a6a8888044d84f85148d783fad67b34
Olgu Sırman <olgu.sirman@vngrs.com>
2018-07-11 10:29:48 +0300

    Initial Commit

---
+++ VNGRSAutolayoutTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNGRSAutolayoutTalk
+//
+//  Created by Olgu Sırman on 11.07.2018.
+//  Copyright © 2018 Olgu Sırman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
